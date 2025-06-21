610d1224be88c1ba91548698d72606f877b83d80
sameersharmadev <sameerkw77@gmail.com>
2025-06-21 14:35:14 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'https://devlog-hmcjvqzxi-sameer-sharmas-projects-ccb3db7d.vercel.app',
-        'https://devlog.sameersharma.me/'
+        'https://devlog.sameersharma.me'
       ],
     credentials: true               
   }));app.use(express.json());
